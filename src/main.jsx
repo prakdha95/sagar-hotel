0fbc1637fd46a909adb809246a19ee2c95be0f13
Prakash <161943231+prakdha95@users.noreply.github.com>
2024-12-06 13:00:11 +0400

    google analytics setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-Y6W7XK83ES');
+
+// Track page views
+const trackPageView = () => {
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
+};
+
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/">
